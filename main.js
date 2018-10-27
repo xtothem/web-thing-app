7125fab79f1b75f67a2e66c5b6aa7dfc376818a0
xtothem <xumingxmz@gmail.com>
2018-10-27 14:34:59 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,99 @@
+
+//get modal button element
+var myModalBtn = document.getElementById("myModalBtn");
+//get modal content element
+var modal = document.getElementById("simpleModal");
+
+var closeBtn = document.getElementById("closeModal");
+
+
+myModalBtn.addEventListener("click",openModal);
+closeBtn.addEventListener("click", closeModal);
+
+function openModal(){
+
+
+		modal.style.display = "block";
+
+
+}
+
+function closeModal(){
+
+		modal.style.display = "none";
+}
+
+
+//get modal button element
+var myModalBtn1 = document.getElementById("myModalBtn1");
+//get modal content element
+var modal1 = document.getElementById("simpleModal1");
+
+var closeBtn1 = document.getElementById("closeModal1");
+
+
+myModalBtn1.addEventListener("click",openModal);
+closeBtn1.addEventListener("click", closeModal);
+
+function openModal(){
+
+
+		modal.style.display = "block";
+
+
+}
+
+function closeModal(){
+
+		modal.style.display = "none";
+}
+
+//get modal button element
+var myModalBtn2 = document.getElementById("myModalBtn2");
+//get modal content element
+var modal2 = document.getElementById("simpleModal2");
+
+var closeBtn2 = document.getElementById("closeModal2");
+
+
+myModalBtn2.addEventListener("click",openModal);
+closeBtn2.addEventListener("click", closeModal);
+
+function openModal(){
+
+
+		modal.style.display = "block";
+
+
+}
+
+function closeModal(){
+
+		modal.style.display = "none";
+}
+
+//get modal button element
+var myModalBtn3 = document.getElementById("myModalBtn3");
+//get modal content element
+var modal3 = document.getElementById("simpleModal3");
+
+var closeBtn3 = document.getElementById("closeModal3");
+
+
+myModalBtn3.addEventListener("click",openModal);
+closeBtn3.addEventListener("click", closeModal);
+
+function openModal(){
+
+
+		modal.style.display = "block";
+
+
+}
+
+function closeModal(){
+
+		modal.style.display = "none";
+}
+
+
